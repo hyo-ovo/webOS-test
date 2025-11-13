fae703c211c122f15cfab05a5b485ae68a7cd3e1
hyo-ovo <hyowonarttech@gmail.com>
2025-11-13 20:34:56 +0900

    fix: ensure webOS media service loads

--- src/frontend/lib/services/media_service.dart
+++ src/frontend/lib/services/media_service.dart
@@ -1,6 +1,6 @@
 import 'media_service_interface.dart';
 import 'media_service_stub.dart'
-    if (dart.library.js) 'media_service_web.dart';
+    if (dart.library.html) 'media_service_web.dart';
 
 export 'media_service_interface.dart';
 

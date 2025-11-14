142d8801eca1a5cfbe171fd976350ab5598579bd
inyoung <rmdnps10@gmail.com>
2025-11-14 14:57:04 +0900

    chore: CORS 임시 설정 #4

--- src/backend/src/server.ts
+++ src/backend/src/server.ts
@@ -33,7 +33,7 @@ const app: Express = express();
 app.set("trust proxy", 1);
 
 app.use(helmet());
-app.use(cors({ origin: env.CORS_ORIGIN, credentials: true }));
+app.use(cors({ origin: true, credentials: true }));
 app.use(rateLimiter);
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
